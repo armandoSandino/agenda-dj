3a2b77ea54ee567055d87e34f2ac4600d0d93598
ArmandoS <jsandino@aimservices.tech>
2020-09-07 19:06:57 +0200

    chore; agregr modelo Hobby y Reunion

--- agenda/applications/persona/models.py
+++ agenda/applications/persona/models.py
@@ -3,7 +3,22 @@ from model_utils.models import TimeStampedModel
 #
 from django.db import models
 
+class Hobby(TimeStampedModel):
 
+    # Definir campos
+    hobby = models.CharField(
+        'Pasa tiempo',
+        max_length=50,
+        blank=False
+    )
+
+    class Meta:
+        verbose_name = 'hobby'
+        verbose_name_plural = 'Hobbies'
+    
+    def __str__(self):
+
+        return self.hobby
 #
 class Person(TimeStampedModel):
     """  Modelo para registrar personas de una agenda  """
@@ -26,11 +41,37 @@ class Person(TimeStampedModel):
         max_length=15,
         blank=True,
     )
-
+    # Relacion many to many,
+    hobbies = models.ManyToManyField(Hobby)
 
     class Meta:
+
         verbose_name = 'Persona'
         verbose_name_plural = 'Personas'
     
     def __str__(self):
         return self.full_name
+
+
+class Reunion(TimeStampedModel):
+    """ Modelo para la reunion """
+    # Clave foranea
+    persona = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE
+    )
+    fecha = models.DateField(blank=False)
+    hora = models.TimeField()
+    asunto = models.CharField(
+        'Asunto de la reunion',
+        max_length=100,
+        blank=False
+    )
+
+    class Meta:
+        verbose_name = 'Reunion'
+        verbose_name_plural = 'Reuniones'
+    
+    def __str__(self):
+        return self.asunto
+    
